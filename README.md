ba789730566380ccc186fa522e34f3c45d1db736
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2018-11-04 17:16:35 -0500

    Search for Dart classes

--- README.md
+++ README.md
@@ -5,13 +5,43 @@ consistent manner.
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+Flutter Stylizer organizes the class(es) within a `*.dart` file
+in the following manner:
 
-For example if there is an image subfolder under your extension project workspace:
+* All constructors are listed first, in sorted order.
+* All factory constructs are listed next, in sorted order.
+* All instance variables are listed next, in sorted order.
+* All `@override` methods are listed next, in sorted order.
+* All other methods are listed next in their original (unsorted) order.
+* The `build` method is listed last.
 
-\!\[feature X\]\(images/feature-x.png\)
+I have found that developer productivity increases when all code in
+large projects follows a consistent and opinionated style.
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+Additionally, bringing new developers into a team with a large code base
+is easier when the code is consistently written and therefore easier
+to navigate and understand.
+
+Without tooling to enforce a consistent style, developing code is less fun.
+Having an automated tool to do this ugly work for you, however, makes
+coding a lot more enjoyable, as you don't have to worry about the rules,
+but can just run the plugin on file save, and the rules are automatically
+enforced.
+
+## Limitations
+
+This plugin does not have a full-featured Dart syntax tree parser.
+As a result, it may come across Dart code that it doesn't handle properly.
+
+It is my goal to be able to use this plugin on large group projects, so
+every attempt has been made to make this robust. If, however, problems
+are found, please raise issues on the GitHub issue tracker for this repo
+along with a (short) example demonstrating the "before" and "after" results
+of running this plugin on the example code.
+
+Even better, please submit a PR with your new "before"/"after" example coded-up
+as a unit test along with the code to fix the problem, and I'll try to
+incorporate the fix into the plugin.
 
 ## Requirements
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,71 @@
 'use strict';
 import * as vscode from 'vscode';
 
+class DartClass {
+    classOffset: number;
+    openCurlyOffset: number;
+    closeCurlyOffset: number;
+    constructor(classOffset: number, openCurlyOffset: number, closeCurlyOffset: number) {
+        this.classOffset = classOffset;
+        this.openCurlyOffset = openCurlyOffset;
+        this.closeCurlyOffset = closeCurlyOffset;
+    }
+}
+
+const matchClassRE = /^class\s+(\S+)\s*.*$/mg;
+
 export function activate(context: vscode.ExtensionContext) {
     console.log('Congratulations, your extension "flutter-stylizer" is now active!');
 
     let disposable = vscode.commands.registerCommand('extension.flutterStylizer', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return; // No open text editor
+        }
+
+        const classes = getClasses(editor.document);
+        console.log("Found " + classes.length.toString() + " classes.");
+        if (classes.length === 0) {
+            return;
+        }
+
         vscode.window.showInformationMessage('Hello World!');
     });
+
+    const getClasses = (document: vscode.TextDocument) => {
+        let classes = new Array<DartClass>();
+        const buf = document.getText();
+        console.log('buf.length=', buf.length);
+        while (true) {
+            let mm = matchClassRE.exec(buf);
+            console.log('mm=', mm);
+            if (!mm) { break; }
+            let className = mm[1];
+            console.log('className=' + className);
+            let classOffset = buf.indexOf(mm[0]);
+            console.log('classOffset=' + classOffset.toString());
+            let openCurlyOffset = findOpenCurlyOffset(buf, classOffset);
+            console.log('openCurlyOffset=', openCurlyOffset);
+            if (openCurlyOffset <= classOffset) {
+                console.log('expected "{" after "class" at offset ' + classOffset.toString());
+                return classes;
+            }
+            let closeCurlyOffset = findMatchingCurly(document, openCurlyOffset);
+            if (closeCurlyOffset <= openCurlyOffset) {
+                console.log('expected "}" after "{" at offset ' + openCurlyOffset.toString());
+            }
+        };
+        return classes;
+    };
+
+    const findOpenCurlyOffset = (buf: string, startOffset: number) => {
+        let offset = buf.substring(startOffset).indexOf('{');
+        return startOffset + offset;
+    };
+
+    const findMatchingCurly = (document: vscode.TextDocument, openCurlyOffset: number) => {
+        return 0;
+    };
 
     context.subscriptions.push(disposable);
 }
