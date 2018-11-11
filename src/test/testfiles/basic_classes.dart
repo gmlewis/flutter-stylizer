1a2e3c67ea9dd6e3594ff67854b806ceec020741
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2018-11-10 19:18:48 -0500

    Add basic_classes.dart

---
+++ src/test/testfiles/basic_classes.dart
@@ -0,0 +1,29 @@
+// This is a test file. After running this through `Flutter Stylizer`,
+// the output should match the content of file `basic_classes_out.dart`.
+
+import 'dart:math';
+
+// Class1 is the first class.
+class Class1 {
+  // _pvi is a private instance variable.
+  List<String> _pvi = ['one', 'two'];
+
+  // _spv is a static private variable.
+  static final String _spv = 'spv';
+
+  // _spvni is a static private variable with no initializer.
+  static double _spvni;
+  int _pvini;
+  static int sv;
+  int v;
+  final double fv = 42.0;
+  Class1();
+  Class1.fromNum();
+  var myfunc = (int n) => n;
+  get vv => v; // getter
+  @override
+  toString() {
+    print('$_pvi, $_spv, $_spvni, $_pvini, ${sqrt(2)}');
+    return '';
+  }
+}
