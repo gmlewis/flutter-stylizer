369b1a3e1749198a0556d6a6d3c010b7228d2bea
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2021-04-14 18:18:17 -0400

    Switch to no semicolons

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,48 +1,49 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
-    },
-    "extends": [],
-    "ignorePatterns": [],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "project": "tsconfig.json",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
+  "env": {
+    "es6": true,
+    "node": true
+  },
+  "extends": [],
+  "ignorePatterns": [],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "project": "tsconfig.json",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "@typescript-eslint"
+  ],
+  "rules": {
+    "@typescript-eslint/class-name-casing": "warn",
+    "@typescript-eslint/semi": [
+      "warn",
+      "never"
     ],
-    "rules": {
-        "@typescript-eslint/class-name-casing": "warn",
-        "@typescript-eslint/semi": [
-            "warn",
-            "always"
-        ],
-        "curly": "warn",
-        "eqeqeq": [
-            "warn",
-            "always"
-        ],
-        "no-throw-literal": "warn",
-        "semi": [
-            "off"
-        ],
-        "@typescript-eslint/member-delimiter-style": [
-            "warn",
-            {
-                "multiline": {
-                    "delimiter": "semi",
-                    "requireLast": true
-                },
-                "singleline": {
-                    "delimiter": "semi",
-                    "requireLast": false
-                }
-            }
-        ],
-        "no-redeclare": "warn",
-        "no-unused-expressions": "warn"
-    },
-    "settings": {}
-};
+    "curly": "warn",
+    "eqeqeq": [
+      "warn",
+      "always"
+    ],
+    "no-throw-literal": "warn",
+    "semi": [
+      "error",
+      "never"
+    ],
+    "@typescript-eslint/member-delimiter-style": [
+      "warn",
+      {
+        "multiline": {
+          "delimiter": "semi",
+          "requireLast": false
+        },
+        "singleline": {
+          "delimiter": "semi",
+          "requireLast": false
+        }
+      }
+    ],
+    "no-redeclare": "warn",
+    "no-unused-expressions": "warn"
+  },
+  "settings": {}
+}
